f04681fe6c6674ba88f4193904dc149c6124eacc
RoderCode <Rodersnow@gmail.com>
2023-09-15 08:27:37 +0200

    first commit

--- src/views/MobilePadView.vue
+++ src/views/MobilePadView.vue
@@ -1,26 +1,17 @@
 <template>
   <!-- HOME -->
   <div>
-    <NavLink name="Home" />
-    <HomeLink />
+    <h1>Hello world</h1>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import baselineSignalWifi1Bar from "~icons/ic/baseline-signal-wifi-1-bar";
-import BaseButtonLink from "../components/base/BaseButtonLink.vue";
-import ButtonMenu from "../components/ButtonMenu.vue";
-import NavLink from "../components/NavLink.vue";
-import HomeLink from "../components/HomeLink.vue";
 
 export default defineComponent({
   components: {
     baselineSignalWifi1Bar,
-    BaseButtonLink,
-    ButtonMenu,
-    NavLink,
-    HomeLink,
   },
   name: "HomeView",
   data() {

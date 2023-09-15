34e63af70b1f844a1b851a1ff48e7a2088d916dd
Marcus <54941923+rodercode@users.noreply.github.com>
2023-09-15 08:24:42 +0200

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- HOME -->
+  <div>
+    <NavLink name="Home" />
+    <HomeLink />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import baselineSignalWifi1Bar from "~icons/ic/baseline-signal-wifi-1-bar";
+import BaseButtonLink from "../components/base/BaseButtonLink.vue";
+import ButtonMenu from "../components/ButtonMenu.vue";
+import NavLink from "../components/NavLink.vue";
+import HomeLink from "../components/HomeLink.vue";
+
+export default defineComponent({
+  components: {
+    baselineSignalWifi1Bar,
+    BaseButtonLink,
+    ButtonMenu,
+    NavLink,
+    HomeLink,
+  },
+  name: "HomeView",
+  data() {
+    return {};
+  },
+});
+</script>
